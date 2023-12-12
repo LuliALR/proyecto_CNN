1990f8c653afe978db0f61c3f257e02d9eaf96b0
Ana Lerones <anelr526@gmail.com>
2023-12-12 01:36:40 -0400

    feat: add prediction function

--- CIFAR100_classifier/functions.py
+++ CIFAR100_classifier/functions.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from keras.models import load_model
 
 def normalize(dataset: np.ndarray) -> np.ndarray:
     """
